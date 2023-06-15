c238dde3049f668d1756931c64020e68dd8fcdfe
배대철 <qoeocjf5@gmail.com>
2023-06-15 13:44:15 +0900

    add: 전역 스타일 theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import 'styles/global.scss';
 
 import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { themes } from '@styles/themes';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={themes}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/themes.ts
@@ -0,0 +1,13 @@
+export const themes = {
+  colors: {
+    PrimaryPurple09: '#5E3DEA',
+    SecondaryGreen: '#00CF58',
+    Gray01: '#F9F9F9', // 배경
+    Gray02: '#E9E9E9', //place holder
+    Gray07: '#999999', //text
+    Red: '#E65052',
+    Black01: '#000000', //text
+    Black02: '#252525', //
+    White: '#FFFFFF',
+  },
+};
