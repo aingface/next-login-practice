ba43cc1505cdd961b15c17a26238bf3bfe676223
배대철 <qoeocjf5@gmail.com>
2023-06-19 20:44:47 +0900

    add: NavigationBar 기능 추가
    - props로 title 입력시 반영
    - props로 isBorderBottom 입력시 반영

--- src/components/UIKit/NavigationBar.tsx
+++ src/components/UIKit/NavigationBar.tsx
@@ -1,28 +1,54 @@
 import arrowLeft from '@assets/icons/icon_arrow-left.png';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import React from 'react';
 import styled from 'styled-components';
 
-function NavigationBar() {
+interface NavigationBarProps {
+  title?: string;
+  isBorderBottom?: boolean;
+}
+
+interface ContainerProps {
+  isBorderBottom?: boolean;
+}
+
+function NavigationBar({ title = '', isBorderBottom = false }: NavigationBarProps) {
   const router = useRouter();
   const handleOnClickBackBtn = () => router.back();
   return (
-    <Container>
+    <Container isBorderBottom={isBorderBottom}>
       <BackButton onClick={handleOnClickBackBtn}>
         <Image alt="My Image" height={24} src={arrowLeft} width={24} />
       </BackButton>
+      <TitleWrapper>
+        <Title>{title}</Title>
+      </TitleWrapper>
     </Container>
   );
 }
 
 export default NavigationBar;
 
-const Container = styled.div`
+const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
-  height: 15vh;
+  flex-direction: row;
+  padding-top: 7vh;
+  padding-bottom: 1vh;
   width: 100vw;
+  border-bottom: 1px solid ${(props) => (props.isBorderBottom ? props.theme.colors.Gray04 : 'none')};
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+const Title = styled.span`
+  font-size: 1.2rem;
+  font-family: Apple SD Gothic Neo, sans-serif;
+  color: ${(props) => props.theme.colors.Black};
 `;
 
 const BackButton = styled.button`
