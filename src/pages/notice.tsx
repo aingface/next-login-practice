c88268d997ed9b15b301db1a99ef83c43941eb89
배대철 <qoeocjf5@gmail.com>
2023-06-28 16:11:43 +0900

    Add: 로그인 성공 메세지

--- src/pages/notice.tsx
+++ src/pages/notice.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import NavigationBar from '@components/UIKit/NavigationBar';
+import styled from 'styled-components';
 
 function notice() {
-  return <div>알림</div>;
+  return (
+    <Container>
+      <NavigationBar isBorderBottom={true} title={'알림'} />
+      <Description>
+        <span>
+          로그인에 <br />
+          <Bold>성공</Bold>을<br />
+          하셨군요<Bold>.</Bold>
+        </span>
+      </Description>
+    </Container>
+  );
 }
 
 export default notice;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${(props) => props.theme.colors.Gray01};
+  width: 100%;
+  height: 100vh;
+  min-height: 100vh;
+  height: fit-content;
+  padding: 0 1.625rem;
+`;
+
+const Description = styled.div`
+  font-family: Apple SD Gothic Neo, sans-serif;
+  font-size: 2rem;
+  font-weight: ${(props) => props.theme.fontWeights.light};
+  padding-top: 2rem;
+  width: 100%;
+
+  margin-top: 7rem;
+`;
+
+const Bold = styled.span`
+  font-weight: ${(props) => props.theme.fontWeights.bold};
+`;
