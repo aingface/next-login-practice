d20e26d1b84d10177f9c6e1f6b74e22e40a4abe7
배대철 <qoeocjf5@gmail.com>
2023-06-28 14:42:13 +0900

    fix: export 분리

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,5 +4,6 @@ import { validateEmail } from './emailUtils';
 import { validatePassword } from './passwordInputUtils';
 import { checkIsAgreeAll, checkIsRequiredConditionsMet } from './termsAndConditionsUtils';
 
-export { checkIsAgreeAll, checkIsRequiredConditionsMet, validateEmail, validatePassword };
+export { checkIsAgreeAll, checkIsRequiredConditionsMet };
+export { validateEmail, validatePassword };
 export { formatDate };
